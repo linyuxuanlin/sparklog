bcd4d22244e8fc6e372eff23ae462db003c8b928
linyuxuanlin <linyuxuanlin@outlook.com>
2025-08-09 13:22:57 +0800

    改进移动侧边栏的logo效果

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { Settings, BookOpen, X } from 'lucide-react'
+import { Settings, BookOpen } from 'lucide-react'
 import { useGitHub } from '@/hooks/useGitHub'
 
 interface SidebarProps {
@@ -24,18 +24,16 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
 
   return (
     <div className="w-64 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 flex flex-col h-full">
-      {/* 移动端关闭按钮 */}
-      <div className="lg:hidden flex justify-between p-4 border-b border-gray-200 dark:border-gray-700 min-h-[7rem]">
-        <div className="text-center flex-1 flex flex-col justify-center px-2">
+      {/* 移动端Logo */}
+      <div className="lg:hidden p-4 border-b border-gray-200 dark:border-gray-700 min-h-[7rem]">
+        <Link 
+          to="/" 
+          onClick={onClose}
+          className="block text-center flex flex-col justify-center h-full px-2"
+        >
           <h1 className="logo-title">SparkLog</h1>
           <p className="logo-subtitle"> </p>
-        </div>
-        <button
-          onClick={onClose}
-          className="p-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg transition-colors"
-        >
-          <X className="w-5 h-5" />
-        </button>
+        </Link>
       </div>
       
       {/* 桌面端Logo */}
